969496669a9913019010122ff98ab2820eec3677
kdp <torinoyume@naver.com>
2019-08-27 12:33:56 +0900

    Update FileValidation.php

--- Validation/FileValidation.php
+++ Validation/FileValidation.php
@@ -4,8 +4,14 @@ namespace Xanax\Validation;
 
 class FileValidation {
 	
+	function hasSubfolderSyntax ( $filePath ) {
+		if( preg_match('/..\/$/i', $filePath) ) {
+			return true;
+		}
+	}
+	
 	function hasExtention ( $filePath ) {
-		if( preg_match('/"^.*\.[A-Za-z0-9]{1,5}"$/i', $filePath) ) {
+		if( preg_match('/^.*\.[A-Za-z0-9]{1,5}$/i', $filePath) ) {
 			return true;
 		}
 		
