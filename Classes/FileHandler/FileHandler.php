b3668db747adabbe4e62703e673db5858a93f9d5
kdp <torinoyume@naver.com>
2019-08-27 12:11:17 +0900

    Update FileHandler.php

--- Classes/FileHandler/FileHandler.php
+++ Classes/FileHandler/FileHandler.php
@@ -5,15 +5,78 @@ use Xanax\Exception\FileHandler\FileIsNotExistsException;
 
 class FileHandler {
 	
-	function Size( $filePath ) :int {
-		if ( $this->isExists($filePath) ) {
-			$return = filesize( $filePath ); // WTF ?? filesize / file_exists
+	function getSize ( $filePath ) :int {
+		if ( !$this->isExists( $filePath ) ) {
+			throw new FileIsNotExistsException ( "File is not exists" );
 		}
+		
+		$return = filesize( $filePath );
 		
 		return $return >= 0 ? $return : -1;
 	}
 	
+	function appendFileContent( $filePath, $content ) {
+		if ( !$this->isExists( $filePath ) ) {
+			throw new FileIsNotExistsException ( "File is not exists" );
+		}
+		
+		$fileHandler = fopen( $filePath, 'a' );
+		fwrite($fileHandler, $content);
+		fclose($fileHandler);
+	}
+	
+	function getLastModifiedTime ( $filePath ) {
+		if ( !$this->isExists( $filePath ) ) {
+			throw new FileIsNotExistsException ( "File is not exists" );
+		}
+		
+		$return = fileatime($filePath);
+		
+		return $return;
+	}
+	
+	function getType ( $filePath ) {
+		if ( !$this->isExists( $filePath ) ) {
+			throw new FileIsNotExistsException ( "File is not exists" );
+		}
+		
+		$return = filetype( $filePath );
+		
+		return $return;
+	}
+	
+	function getExtention ( $filePath ) {
+		if ( !$this->isExists( $filePath ) ) {
+			throw new FileIsNotExistsException ( "File is not exists" );
+		}
+		
+		$return = pathinfo($filePath, PATHINFO_EXTENSION);
+		
+		return $return;
+	}
+	
+	function Download ( $filePath ) {
+		if ( !$this->isExists( $filePath ) ) {
+			throw new FileIsNotExistsException ( "File is not exists" );
+		}
+		
+		$fileHandler = @fopen($filePath, 'rb');
+		if ($fileHandler) {
+			while(!feof($fileHandler)) {
+				print(@fread($fileHandler, 1024 * 8));
+				ob_flush();
+				flush();
+			}
+		}
+		
+		fclose($file);
+	}
+	
 	function getInterpretedContent ( $filePath ) {
+		if ( !$this->isExists( $filePath ) ) {
+			throw new FileIsNotExistsException ( "File is not exists" );
+		}
+		
 		ob_start();
 			
 		if (isset( $filePath )) {
@@ -29,25 +92,41 @@ class FileHandler {
 		return $return;
 	}
 	
-	function isFile( $filePath ) :bool {
+	function isFile ( $filePath ) :bool {
+		if ( !$this->isExists( $filePath ) ) {
+			throw new FileIsNotExistsException ( "File is not exists" );
+		}
+		
 		$return = is_file ( $filePath );
 		
 		return $return;
 	}
 	
-	function Move( $source, $destination ) :bool {
+	function Move ( $source, $destination ) :bool {
+		if ( !$this->isExists( $filePath ) ) {
+			throw new FileIsNotExistsException ( "File is not exists" );
+		}
+		
 		$return = rename( $source, $destination );
 		
 		return $return;
 	}
 	
-	function isEmpty($filePath) :bool {
+	function isEmpty ( $filePath ) :bool {
+		if ( !$this->isExists( $filePath ) ) {
+			throw new FileIsNotExistsException ( "File is not exists" );
+		}
+		
 		$return = $this->Size( $filePath ) !== 0;
 		
 		return $return;
 	}
 	
-	function isExists( $filePath ) :bool {
+	function isExists ( $filePath ) :bool {
+		if ( !$this->isExists( $filePath ) ) {
+			throw new FileIsNotExistsException ( "File is not exists" );
+		}
+		
 		$return = file_exists( $filePath );
 		
 		return $return;
