c43ab40da7644fc34815a1d271c981c6df43b514
kdp <torinoyume@naver.com>
2019-08-27 12:43:48 +0900

    Create FileHandlerMessage.php

---
+++ Message/FileHandler/FileHandlerMessage.php
@@ -0,0 +1,18 @@
+<?php
+namespace Message;
+
+class FileHandlerMessage {
+	
+	function getTargetIsNotFileMessage () {
+		return "Target is not File";
+	}
+	
+	function getDoNotUseSubDirectorySyntaxMessage () {
+		return "Do not use SubDirectory Syntax stupid, do you have a Intellectual disability?";
+	}
+	
+	function getFileIsNotExistsMessage () {
+		return "File is not Exists";
+	}
+	
+}
