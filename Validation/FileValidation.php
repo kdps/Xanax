9d84ddd70603707116ff7f2cadd0d5a9319f3ebf
exuscms <exuscms@gmail.com>
2019-08-27 17:16:08 +0900

    Master

--- Validation/FileValidation.php
+++ Validation/FileValidation.php
@@ -14,28 +14,64 @@ class FileValidation {
 		return true;
 	}
 	
-	function isPharProtocol ( $filePath ) {
-		if( preg_match('/^phar:\/\/.*/i', $filePath) ) {
+	public function isNotReadable ( $filename ) {
+		return ! $this->isReadable ( $filename );
+	}
+	
+	function isHTTPProtocol ( $filePath ) {
+		$regexr = '/^(http||https):\/\//i';
+		
+		if( preg_match( $regexr, $filePath ) ) {
 			return true;
 		}
 		
 		return false;
+	}
+	
+	function isNotHTTPProtocol ( $filePath ) {
+		return ! $this->isHTTPProtocol ( $filePath );
+	}
+	
+	function isPharProtocol ( $filePath ) {
+		$regexr = '/^phar:\/\/.*/i';
+		
+		if( preg_match( $regexr, $filePath ) ) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	function isNotPharProtocol ( $filePath ) {
+		return ! $this->isPharProtocol ( $filePath );
 	}
 	
 	function hasSubfolderSyntax ( $filePath ) {
-		if( preg_match('/..\/$/i', $filePath) ) {
+		$regexr = '/..\/$/i';
+		
+		if( preg_match( $regexr, $filePath ) ) {
 			return true;
 		}
 		
 		return false;
 	}
 	
+	function hasNotSubfolderSyntax ( $filePath ) {
+		return ! $this->hasSubfolderSyntax ( $filePath );
+	}
+	
 	function hasExtention ( $filePath ) {
-		if( preg_match('/^.*\.[A-Za-z0-9]{1,5}$/i', $filePath) ) {
+		$regexr = '/^.*\.[A-Za-z0-9]{1,5}$/i';
+		
+		if( preg_match( $regexr, $filePath ) ) {
 			return true;
 		}
 		
 		return false;
+	}
+	
+	function hasNotExtention ( $filePath ) {
+		return ! $this->hasExtention ( $filePath );
 	}
 	
 }
