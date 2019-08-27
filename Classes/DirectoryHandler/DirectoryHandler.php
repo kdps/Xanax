1f5759cd49e5b838c175600383ab70658f4ef86d
kdp <torinoyume@naver.com>
2019-08-27 12:56:19 +0900

    Create DirectoryHandler.php

---
+++ Classes/DirectoryHandler/DirectoryHandler.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Xanax\Classes;
+
+use Xanax\Exception\Stupid\StupidIdeaException;
+use Xanax\Exception\DirectoryeHandler\DirectoryIsNotExistsException;
+use Xanax\Validation\DirectoryeHandler;
+use Xanax\Message\DirectoryeHandlerMessage;
+
+class DirectoryHandler {
+
+}
