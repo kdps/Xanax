688fedcb2716f1179f712541061e01eeed7bde35
kdp <torinoyume@naver.com>
2019-08-27 15:31:38 +0900

    Update DirectoryHandler.php

--- Classes/DirectoryHandler/DirectoryHandler.php
+++ Classes/DirectoryHandler/DirectoryHandler.php
@@ -2,6 +2,7 @@
 
 namespace Xanax\Classes;
 
+use Xanax\Classes\FileHandler;
 use Xanax\Exception\Stupid\StupidIdeaException;
 use Xanax\Exception\DirectoryeHandler\DirectoryIsNotExistsException;
 use Xanax\Validation\DirectoryeHandler;
@@ -9,4 +10,62 @@ use Xanax\Message\DirectoryeHandlerMessage;
 
 class DirectoryHandler {
 
+	function isDirectory ( $directoryPath ) {
+		$return = is_dir( $directoryPath );
+		
+		return $return;
+	}
+	
+	function Make ( $directoryPath ) {
+		$this->Create( $directoryPath );
+	}
+	
+	function Create ( $directoryPath ) {
+		$return = mkdir( $directoryPath );
+		
+		return $return;
+	}
+	
+	function isEmpty ( $directoryPath ) {
+		$iterator = new RecursiveDirectoryIterator( $directoryPath, FilesystemIterator::SKIP_DOTS );
+        $return = (iterator_count($iterator) === 0) ? true : false;
+		
+		return $return;
+	}
+	
+	function Delete ( $directoryPath ) {
+		if ( $this->isEmpty( $directoryPath ) || $this->Empty ($directoryPath) ) {
+			rmdir ( $directoryPath );
+		} else {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	function Empty ( $directoryPath ) {
+		if ( !$this->isDirectory( $directoryPath ) ) {
+			
+		}
+		
+		$iterator = new RecursiveIteratorIterator(
+			new RecursiveDirectoryIterator( $directoryPath, RecursiveDirectoryIterator::SKIP_DOTS ),
+			RecursiveIteratorIterator::CHILD_FIRST
+		);
+
+		foreach($iterator as $fileinfo) {
+			if($fileinfo->isDir()) {
+				if(delete($fileinfo->getRealPath()) === false) {
+					return false;
+				}
+			} else {
+				if(unlink($fileinfo->getRealPath()) === false) {
+					return false;
+				}
+			}
+		}
+		
+		return true;
+	}
+	
 }
