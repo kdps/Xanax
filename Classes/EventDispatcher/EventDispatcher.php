f989810793b0add9f4762da3387075246769bac5
exuscms <exuscms@gmail.com>
2019-08-30 14:49:22 +0900

    Master

--- Classes/EventDispatcher/EventDispatcher.php
+++ Classes/EventDispatcher/EventDispatcher.php
@@ -5,13 +5,54 @@ namespace Xanax\Classes;
 class EventDispatcher {
 
 	private $listeners = [];
-
-	public function Listen( string $event, callable $listener ) {
-		$this->listeners[ $event ][] = $listener;
+	
+	public function removeListener( string $eventName, callable $listener ) {
+		if ( $this->hasListener( $eventName ) ) {
+			
+		}
 	}
 	
-	public function Emit( object $event ) {
-		$listeners = $this->listeners[ get_class( $event ) ] ?? [];
+	public function getListeners ($eventName = '') {
+		return isset($eventName) ? $this->listeners[$eventName] : $this->listeners;
+	}
+	
+	public function getListenerCount ($listeners) :int {
+		return count ($listeners || $this->getListeners());
+	}
+	
+	public function hasListener ( string $eventName ) {
+		if ( $eventName !== null ) {
+			$listener = $this->getListeners( $eventName );
+			return !empty($listener);
+		}
+		
+		if ( $this->getListenerCount() <= 0 ) {
+			return false;
+		}
+		
+		foreach ( $this->getListeners() as $listenerItem ) {
+			if ( $listenerItem ) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public function addListener ( string $eventName, callable $listener ) {
+		if ( $this->hasListener( $eventName ) ) {
+			return;
+		}
+		
+		$this->listeners[ $eventName ][] = $listener;
+	}
+	
+	public function Emit ( object $event ) {
+		$listeners = $this->getListeners( get_class( $event ) ) ?? [];
+		
+		if ( count($listeners) <= 0 ) {
+			return;
+		}
 		
 		foreach ($listeners as $listener) {
 			$listener($event);
