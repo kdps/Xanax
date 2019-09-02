a70d1555227118fd6f6bb2271e8d8fd609181c76
exuscms <exuscms@gmail.com>
2019-09-02 15:09:33 +0900

    Master

---
+++ Application/Configure/Configure.php
@@ -0,0 +1,8 @@
+<?php
+
+$configure['rootDirectory'] = ".";
+$configure['indexPage'] = "index.php";
+$configure['Language'] = "english";
+$configure['CharacterSet'] = "UTF-8";
+$configure['SessionDriver'] = 'files';
+$configure['SessionDriver'] = 'files';
---
+++ Application/Configure/Database.php
@@ -0,0 +1,15 @@
+<?php
+
+$Database['default'] = array (
+	'DataSourceName' =>    '',
+	'Database' =>          '',
+	'HostName' =>          'localhost',
+	'UserName' =>          '',
+	'Password' =>          '',
+	'DatabaseDriver' =>    'PDO', // Only PDO support
+	'DatabasePrefix' =>    '',
+	'CacheON' =>           FALSE,
+	'CacheDirectory' =>    '',
+	'CharacterSet' =>      'utf8',
+	'DatabaseCollation' => 'utf8_general_ci'
+);
