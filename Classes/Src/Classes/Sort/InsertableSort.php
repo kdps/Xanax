0a5a4e5c7d29ec7eb82eaffa813247fb61ff4c94
exuscms <exuscms@gmail.com>
2019-09-03 14:43:35 +0900

    Update InsertableSort.php

--- Classes/Src/Classes/Sort/InsertableSort.php
+++ Classes/Src/Classes/Sort/InsertableSort.php
@@ -7,53 +7,23 @@ class InsertableSort {
 	public function Sort ( array $array ) {
 		$count = count($array);
 		
-		if ($count < 80) {
-			// Approximately 5~20000 times faster than traditional algorithms
-			for($iterator = 0; $iterator < $count - 1; $iterator++ ) {
-				$isSorted = true;
-				for($index = $iterator; $index > -1; $index-- ) {
-					if ($array[$index] < $array[$index+1]) {
-						$tmp = $array[$index+1];
-						$array[$index+1] = $array[$index];
-						$array[$index] = $tmp;
-						$isSorted = false;
-					}
-				}
-				
-				if ($isSorted) {
-					$iterator = ++$iterator;
+		for($iterator = 0; $iterator < $count - 1; $iterator++ ) {
+			$isSorted = true;
+			for($index = $iterator; $index > -1; $index-- ) {
+				if ($array[$index] < $array[$index+1]) {
+					$tmp = $array[$index+1];
+					$array[$index+1] = $array[$index];
+					$array[$index] = $tmp;
+					$isSorted = false;
 				}
 			}
 			
-			$array = array_reverse($array);
-			
-			$count = count($array);
-			for($iterator = 0; $iterator < $count - 1; $iterator++ ) {
-				for($index = $iterator; $index > -1; $index-- ) {
-					if ($array[$index] < $array[$index+1]) {
-						$tmp = $array[$index+1];
-						$array[$index+1] = $array[$index];
-						$array[$index] = $tmp;
-					}
-				}
+			if ($isSorted && $iterator > $count) {
+				$iterator += 1;
 			}
-			
-			return array_reverse($array);
-		} else {
-			for($iterator = 0; $iterator < $count - 1; $iterator++ ) {
-				$isSorted = true;
-				for($index = $iterator; $index > -1; $index-- ) {
-					if ($array[$index] < $array[$index+1]) {
-						$tmp = $array[$index+1];
-						$array[$index+1] = $array[$index];
-						$array[$index] = $tmp;
-						$isSorted = false;
-					}
-				}
-			}
-			
-			return array_reverse($array);
 		}
+		
+		return array_reverse($array);
 	}
 	
 }
