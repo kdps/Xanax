25b66586499df7df6bdea0f20ab3604a67a5d90d
exuscms <exuscms@gmail.com>
2019-09-08 04:06:05 +0900

    Master

--- Classes/Src/Classes/Interpreter/Tokenizer.php
+++ Classes/Src/Classes/Interpreter/Tokenizer.php
@@ -11,13 +11,18 @@ class Tokenizer {
 	const PARENTHESIES = 2;
 	const WORD = 3;
 	const COMMA = 4;
+	const DATATYPE = 5;
 	
 	private $tokens = array();
 	private $token = "";
 	
 	private $variableSpecialCharacter = array("-", "_");
+	
+	private $quotes = array('"', "'");
+	
 	private $operatorTokens = array(
 		"," => self::COMMA,
+		"?" => self::OPERATOR,
 		"=" => self::OPERATOR,
 		"&" => self::OPERATOR,
 		"%" => self::OPERATOR,
@@ -30,11 +35,24 @@ class Tokenizer {
 		"(" => self::PARENTHESIES,
 		")" => self::PARENTHESIES
 	);
-		
+	private $firstTokens = array(
+		"string" => self::DATATYPE,
+		"integer" => self::DATATYPE,
+		"boolean" => self::DATATYPE,
+		"float" => self::DATATYPE,
+		"array" => self::DATATYPE,
+		"double" => self::DATATYPE,
+		"long" => self::DATATYPE,
+		"character" => self::DATATYPE,
+		"short" => self::DATATYPE
+	);
+	
 	const STATE_DEFAULT = 0;
 	const STATE_STRING = 1;
-	const STATE_NUMBER = 1;
-	const STATE_COMMENT = 1;
+	const STATE_NUMBER = 2;
+	const STATE_COMMENT = 3;
+	const STATE_WORD = 4;
+	const STATE_OPERATOR = 5;
 	
 	public function generateToken ( $string ) {
 		$count = strlen( $string );
@@ -43,33 +61,62 @@ class Tokenizer {
 		
 		// Lexical analysis
 		for ($i = 0; $i < $count; $i++) {
-			$character = $string{$i};
+			$character = $string[$i];
 			
 			switch ( $state ) {
 				case self::STATE_DEFAULT:
 					if (ctype_alpha($character)) {
 						$this->token .= $character;
-						$state = self::STATE_STRING;
-					} else if (is_numeric($character)) {
+						$state = self::STATE_WORD;
+					} else if ( is_numeric($character) ) {
 						$this->token .= $character;
-						$state = self::STATE_STRING;
+						$state = self::STATE_WORD;
 					} else if ( in_array ($character, $this->variableSpecialCharacter) ) {
 						$this->token .= $character;
 					} else if ( isset($this->operatorTokens[$character]) ) {
-						$this->tokens[] = new TokenObject($character, $this->operatorTokens[$character]);
+						$this->token .= $character;
+						$state = self::STATE_OPERATOR;
+					} else if ( in_array ($character, $this->quotes) ) {
+						$state = self::STATE_STRING;
 					}
 					
-					if ($character === '"') {
+					break;
+				case self::STATE_OPERATOR:
+					if ( isset($this->operatorTokens[$character]) ) {
+						$this->token .= $character;
+					} else {
+						$this->tokens[] = new TokenObject($this->token, $this->operatorTokens[$this->token]);
+						$this->token = "";
+						$i--;
+						
+						$state = self::STATE_DEFAULT;
 					}
 					
 					break;
 				case self::STATE_STRING:
+					if ($character == '"') {
+						$this->tokens[] = new TokenObject($this->token, self::STATE_STRING);
+						$this->token = "";
+						
+						$state = self::STATE_DEFAULT;
+					} else {
+						$this->token .= $character;
+					}
+					
+					break;
+				case self::STATE_WORD:
 					if (ctype_alnum($character) || in_array ($character, $this->variableSpecialCharacter) ) {
 						$this->token .= $character;
 					} else {
-						$this->tokens[] = new TokenObject($this->token, self::WORD);
+						if ( isset($this->operatorTokens[$character]) ) {
+							$this->tokens[] = new TokenObject($this->token, self::STATE_WORD);
+						} else {
+							$this->tokens[] = new TokenObject($this->token, self::STATE_WORD);
+						}
+						
 						$this->token = "";
 						$i--;
+						
 						$state = self::STATE_DEFAULT;
 					}
 					
