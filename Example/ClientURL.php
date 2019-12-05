2c3dd14984ff6d14548ffc906fc45c6a20d490a7
정승열 <kdp7584@kodc.co.kr>
2019-12-05 18:38:35 +0900

    Update ClientURL.php

--- Example/ClientURL.php
+++ Example/ClientURL.php
@@ -5,6 +5,7 @@ include './../vendor/autoload.php';
 use Xanax\Classes\ClientURL;
 
 $cURL = new ClientURL();
-$cURL->Option->setURL("localhost");
+$cURL->Option->setURL('localhost');
 $cURL->Option->setReturnTransfer(true);
+
 echo $cURL->Execute();
