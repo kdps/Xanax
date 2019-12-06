ccd73789c29118442e174bffc734e8f63c1dfa2e
정승열 <kdp7584@kodc.co.kr>
2019-12-06 13:24:24 +0900

    Master

--- Classes/Src/Interface/File/FileHandlerInterface.php
+++ Classes/Src/Interface/File/FileHandlerInterface.php
@@ -4,69 +4,73 @@ namespace Xanax\Implement;
 
 interface FileHandlerInterface
 {
-    public function isFile(string $filePath, string $containDirectory = null) :bool;
+	public function isFile(string $filePath, string $containDirectory = null) :bool;
 
-    public function isEmpty(string $filePath) :bool;
+	public function isEmpty(string $filePath) :bool;
 
-    public function isExists(string $filePath) :bool;
+	public function isExists(string $filePath) :bool;
 
-    public function isContainFolder(string $basePath, string $filePath) :bool;
+	public function isContainFolder(string $basePath, string $filePath) :bool;
 
-    public function isCorrectInode($filePath) :bool;
+	public function isCorrectInode($filePath) :bool;
 
-    public function isValidHandler($fileHandler);
+	public function isValidHandler($fileHandler);
 
-    public function isReadable(string $filePath) :bool;
+	public function isReadable(string $filePath) :bool;
 
-    public function isLocked($filePath) :bool;
+	public function isLocked($filePath) :bool;
 
-    public function isRegularFile(string $filePath) :bool;
+	public function isRegularFile(string $filePath) :bool;
 
-    public function isSymbolicLink(string $filePath) :bool;
+	public function isSymbolicLink(string $filePath) :bool;
 
-    public function isUnknownFile(string $filePath) :bool;
+	public function isUnknownFile(string $filePath) :bool;
 
-    public function isEqualByLine(string $filePath, string $string = null) :bool;
+	public function isEqualByLine(string $filePath, string $string = null) :bool;
 
-    public function isWritable(string $filePath) :bool;
+	public function isWritable(string $filePath) :bool;
 
-    public function getSize(string $filePath, bool $humanReadable) :int;
+	public function getSize(string $filePath, bool $humanReadable) :int;
 
-    public function getInode(string $filePath);
+	public function getInode(string $filePath);
 
-    public function getLastModifiedTime(string $filePath) :string;
+	public function getLastModifiedTime(string $filePath) :string;
 
-    public function getType(string $filePath) :string;
+	public function getCreatedDate($filePath);
 
-    public function getExtention(string $filePath) :string;
+	public function getLastAccessDate($filePath);
 
-    public function getBasename(string $fileName, $extension = null) :string;
+	public function getType(string $filePath) :string;
 
-    public function getContent(string $filePath) :string;
+	public function getExtention(string $filePath) :string;
 
-    public function getTypeByHeader(string $filePath) :string;
+	public function getBasename(string $fileName, $extension = null) :string;
 
-    public function getInterpretedContent(string $filePath) :string;
+	public function getContent(string $filePath) :string;
 
-    public function Merge(string $filePath, string $mergeFile) :bool;
+	public function getTypeByHeader(string $filePath) :string;
 
-    public function Delete(string $filePath) :bool;
+	public function getInterpretedContent(string $filePath) :string;
 
-    public function copy(string $filePath, string $destinationPath) :bool;
+	public function Merge(string $filePath, string $mergeFile) :bool;
 
-    public function appendContent(string $filePath, string $content = null, bool $overwrite = true) :bool;
+	public function Delete(string $filePath) :bool;
 
-    public function Write(string $filePath, string $content = null, string $writeMode = 'w') :bool;
+	public function copy(string $filePath, string $destinationPath) :bool;
 
-    public function Read(string $filePath, int $length = -1, string $writeMode = 'r');
+	public function appendContent(string $filePath, string $content = null, bool $overwrite = true) :bool;
 
-    public function readAllContent(string $filePath, string $writeMode = 'r');
+	public function Write(string $filePath, string $content = null, string $writeMode = 'w') :bool;
 
-    public function reverseContent(string $filePath) :bool;
+	public function Read(string $filePath, int $length = -1, string $writeMode = 'r');
 
-    public function Download(string $filePath) :bool;
+	public function readAllContent(string $filePath, string $writeMode = 'r');
 
-    public function requireOnce(string $filePath);
+	public function reverseContent(string $filePath) :bool;
 
-    public function Move(string $source, string $destination) :bool;
+	public function Download(string $filePath) :bool;
+
+	public function requireOnce(string $filePath);
+
+	public function Move(string $source, string $destination) :bool;
 }
