5cf46e6a21ae3eaa1e38de595329fdd30b090369
kdps <torinoyume@naver.com>
2019-12-31 17:28:13 +0900

    MIME

--- Classes/Src/Classes/MIME/MIME.php
+++ Classes/Src/Classes/MIME/MIME.php
@@ -9,16 +9,178 @@ class MIME
 	
 	private static $extension = '';
 	
-	// https://doc.wikimedia.org/mediawiki-core/master/php/MimeAnalyzer_8php_source.html
 	private $mimeTypes = [
+	
+		/* Text */
+	
+		'html' => [
+			'mimeType' => 'text/html'
+		],
+		'csv' => [
+			'mimeType' => 'text/csv'
+		],
+		'css' => [
+			'mimeType' => 'text/css'
+		],
+		'text' => [
+			'mimeType' => 'text/plain'
+		],
+		'ics' => [
+			'mimeType' => 'text/calendar'
+		],
+		
+		/* Image */
+	
+		'tif' => [
+			'mimeType' => 'image/tiff'
+		],
+		'tiff' => [
+			'mimeType' => 'image/tiff'
+		],
+		'gif' => [
+			'mimeType' => 'image/gif'
+		],
+		'png' => [
+			'mimeType' => 'image/png'
+		],
+		'svg' => [
+			'mimeType' => 'image/svg+xml'
+		],
+		'ico' => [
+			'mimeType' => 'image/x-icon'
+		],
+		'webp' => [
+			'mimeType' => 'image/webp'
+		],
+		'bmp' => [
+			'mimeType' => 'image/x-bmp'
+		],
+		'djvu' => [
+			'mimeType' => 'image/vnd.djvu'
+		],
+		'ppm' => [
+			'mimeType' => 'image/x-portable-pixmap'
+		],
+		'xcf' => [
+			'mimeType' => 'image/x-xcf'
+		],
+		
+		/* Video */
+		
+		'avi' => [
+			'mimeType' => 'video/x-msvideo'
+		],
+		'mpeg' => [
+			'mimeType' => 'video/mpeg'
+		],
+		'webm' => [
+			'mimeType' => 'video/webm'
+		],
+		'ogg' => [
+			'mimeType' => 'video/ogg'
+		],
+		
+	
+		/* Audio */
+		
+		'oga' => [
+			'mimeType' => 'audio/ogg'
+		],
+		'weba' => [
+			'mimeType' => 'audio/webm'
+		],
+		'aac' => [
+			'mimeType' => 'audio/aac'
+		],
+		'wav' => [
+			'mimeType' => 'audio/x-wav'
+		],
+		'mid' => [
+			'mimeType' => 'audio/midi'
+		],
+		'midi' => [
+			'mimeType' => 'audio/midi'
+		],
+		
+		/* Compression */
+		
+		'tar' => [
+			'mimeType' => 'application/x-tar'
+		],
+		'bz2' => [
+			'mimeType' => 'application/x-bzip2'
+		],
+		'bz' => [
+			'mimeType' => 'application/x-bzip'
+		],
+		'zip' => [
+			'mimeType' => 'application/zip'
+		],
+		'arc' => [
+			'mimeType' => 'application/octet-stream'
+		],
+		'rar' => [
+			'mimeType' => 'application/x-rar-compressed'
+		],
+		'7z' => [
+			'mimeType' => 'application/x-7z-compressed'
+		],
+		
+		/* Application */
+		
+		'abw' => [
+			'mimeType' => 'application/x-abiword'
+		],
+		'mpkg' => [
+			'mimeType' => 'application/vnd.apple.installer+xml'
+		],
+		'csh' => [
+			'mimeType' => 'application/x-csh'
+		],
+		'jar' => [
+			'mimeType' => 'application/java-archive'
+		],
 		'pdf' => [
-				'mimeType' => 'application/pdf'
-			]
+			'mimeType' => 'application/pdf'
+		],
+		'ttf' => [
+			'mimeType' => 'application/x-font-ttf'
+		],
+		'swf' => [
+			'mimeType' => 'application/x-shockwave-flash'
+		],
+		'odt' => [
+			'mimeType' => 'application/vnd.oasis.opendocument.text'
+		],
+		'excel' => [
+			'mimeType' => 'application/vnd.ms-excel'
+		],
+		'azw' => [
+			'mimeType' => 'application/vnd.amazon.ebook'
+		],
+		'binary' => [
+			'mimeType' => 'application/octet-stream'
+		],
+		'json' => [
+			'mimeType' => 'application/json'
+		],
+		'woff' => [
+			'mimeType' => 'application/x-font-woff'
+		],
+		'xhtml' => [
+			'mimeType' => 'application/xhtml+xml'
+		],
+		'ppt' => [
+			'mimeType' => 'application/vnd.ms-powerpoint'
+		],
+		'xml' => [
+			'mimeType' => 'application/xml'
+		]
 	];
 	
 	public function __construct($extension = '')
 	{
-		if ($extension) {
+		if ( $extension ) {
 			self::$extension = $extension;
 		}
 	}
@@ -30,74 +192,10 @@ class MIME
 	
 	public function getType($extension = '')
 	{
-		if (!$extension && !self::$extension) {
-			$extension = self::$extension;
-		}
-		
 		$mimeType = '';
 		
-		// https://developer.mozilla.org/ko/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-		switch ($extension) {
-			case 'aac':
-				$mimeType = 'audio/aac';
-				break;
-			case 'arc':
-				$mimeType = 'application/octet-stream';
-				break;
-			case 'zip':
-				$mimeType = 'application/zip';
-				break;
-			case 'swf':
-				$mimeType = 'application/x-shockwave-flash';
-				break;
-			case 'ttf':
-				$mimeType = 'application/x-font-ttf';
-				break;
-			case 'svg':
-				$mimeType = 'image/svg+xml';
-				break;
-			case 'odt':
-				$mimeType = 'application/vnd.oasis.opendocument.text';
-				break;
-			case 'wav':
-				$mimeType = 'audio/x-wav';
-				break;
-			case 'excel':
-				$mimeType = 'application/vnd.ms-excel';
-				break;
-			case 'rar':
-				$mimeType = 'application/x-rar-compressed';
-				break;
-			case 'azw':
-				$mimeType = 'application/vnd.amazon.ebook';
-				break;
-			case '7z':
-				$mimeType = 'application/x-7z-compressed';
-				break;
-			case 'xml':
-				$mimeType = 'application/xml';
-				break;
-			case 'json':
-				$mimeType = 'application/json';
-				break;
-			case 'binary':
-				$mimeType = 'application/octet-stream';
-				break;
-			case 'png':
-				$mimeType = 'image/png';
-				break;
-			case 'gif':
-				$mimeType = 'image/gif';
-				break;
-			case 'text':
-				$mimeType = 'text/plain';
-				break;
-			case 'pdf':
-				$mimeType = 'application/pdf';
-				break;
-			default:
-				$mimeType = $applicationType;
-				break;
+		if ( isset( $this->mimeTypes[ $extension ] ) ) {
+			$mimeType = $this->mimeTypes[ $extension ][ 'mimeType' ];
 		}
 		
 		return $mimeType;
