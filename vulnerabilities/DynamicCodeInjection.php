be5d5ab27beb48403c75b949c4ca1fd26ae3356e
kdps <torinoyume@naver.com>
2019-12-31 18:33:30 +0900

    Remove Unused File

--- vulnerabilities/DynamicCodeInjection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_GET['func'] = "system";
-$_GET['argument'] = "uname";
-
-$func = $_GET['func'];
-$argument = $_GET['argument'];
-$func($argument);
-?>
--- vulnerabilities/array_diff_uassoc array_diff_ukey array_intersect_uassoc array_intersect_ukey.php
+++
@@ -1,9 +0,0 @@
-<?php
-$_GET['arg1'] = "phpinfo()";
-$_GET['arg2'] = "assert";
-$_GET['func'] = "call_user_func";
-
-$some = array($_GET['arg1'] =>1, $_GET['arg2']=>2);
-$func = $_GET['func'];
-array_diff_ukey($some, array(), $func);
-?>
