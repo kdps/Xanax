200c4dc48ad8363cc834c29108fd13398721c53b
kdp <torinoyume@naver.com>
2020-01-03 12:58:44 +0900

    Update Permission.php

--- Classes/Src/Classes/Permission/Permission.php
+++ Classes/Src/Classes/Permission/Permission.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Xanax\Classes;
 
+use Xanax\Classes\Permission\Group as Group;
+use Xanax\Classes\Permission\Owner as Owner;
+use Xanax\Classes\Permission\World as World;
+
 class Permission
 {
   private static $mode;
@@ -12,5 +16,5 @@ class Permission
   {
     self::$mode = $mode;
   }
-  
+
 }
