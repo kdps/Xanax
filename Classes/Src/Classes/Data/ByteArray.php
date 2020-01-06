b22e10ccfa1dbec5ee8102b30bb666346430c9b2
kdp <torinoyume@naver.com>
2020-01-06 13:09:28 +0900

    Create ByteArray.php

---
+++ Classes/Src/Classes/Data/ByteArray.php
@@ -0,0 +1,15 @@
+<?php
+
+class ByteArray
+{
+  public function toString()
+  {
+    return implode(array_map("chr", parent::$data));
+  }
+  
+  public function toByteArray()
+  {
+    parent::$data = unpack('C*', parent::$data)
+  }
+  
+}
