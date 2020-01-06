8415994c114873bf0e77b8f7396815647d99a6ca
kdp <torinoyume@naver.com>
2020-01-06 19:43:13 +0900

    Update Regex.php

--- Classes/Src/Classes/Regex/Regex.php
+++ Classes/Src/Classes/Regex/Regex.php
@@ -5,8 +5,44 @@ namespace Xanax/Classes;
 class Regex
 {
 
+  public function getErrorCode()
+  {
+    return preg_last_error();
+  }
+  
+  public function hasJITStackLimitError()
+  {
+    return PREG_JIT_STACKLIMIT_ERROR !== $this->getErrorCode();
+  }
+  
+  public function hasBadUTF8OffsetError()
+  {
+    return PREG_BAD_UTF8_OFFSET_ERROR !== $this->getErrorCode();
+  }
+  
+  public function hasBadUTF8Error()
+  {
+    return PREG_BAD_UTF8_ERROR !== $this->getErrorCode();
+  }
+  
+  public function hasRecursionLimitEror()
+  {
+    return PREG_RECURSION_LIMIT_ERROR !== $this->getErrorCode();
+  }
+  
+  public function hasBacktrackLimitError()
+  {
+    return PREG_BACKTRACK_LIMIT_ERROR !== $this->getErrorCode();
+  }
+  
+  public function hasInternalError()
+  {
+    return PREG_INTERNAL_ERROR !== $this->getErrorCode();
+  }
+  
   public function hasError()
   {
-    return PREG_NO_ERROR === preg_last_error();
+    return PREG_NO_ERROR !== $this->getErrorCode();
   }
+  
 }
