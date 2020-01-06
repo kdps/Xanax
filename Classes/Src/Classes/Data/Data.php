40bcfb44304e9c23fdf6b3cccabb39885d30e30e
kdp <torinoyume@naver.com>
2020-01-06 12:36:20 +0900

    Update Data.php

--- Classes/Src/Classes/Data/Data.php
+++ Classes/Src/Classes/Data/Data.php
@@ -2,6 +2,15 @@
 
 namespace Xanax/Classes;
 
+use Xanax/Classes/Data/Identifier as Identifier;
+
 class Data
 {
+  private static $data;
+  
+  public function __constructor($data)
+  {
+    self::$data = $data;
+  }
+  
 }
