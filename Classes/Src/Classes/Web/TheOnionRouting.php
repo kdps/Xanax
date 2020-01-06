ecfbcf085b5dc347580ef75ec6071201a082d3a5
kdp <torinoyume@naver.com>
2020-01-06 17:46:08 +0900

    Update TheOnionRouting.php

--- Classes/Src/Classes/Web/TheOnionRouting.php
+++ Classes/Src/Classes/Web/TheOnionRouting.php
@@ -3,22 +3,27 @@
 namespace Xanax/Classes;
 
 use Xanax/Classes/InternetProtocol as InternetProtocol;
+use Xanax/Classes/RequestHandler as RequestHandler;
 
 class TheOnionRouting
 {
 
   private static $internetProtocolClass;
+  private static $requestHandlerClass;
 
   public function __constructor()
   {
     self::$internetProtocolClass = new InternetProtocol();
+    self::$requestHandlerClass = new RequestHandler();
   }
   
   public function isExitNode()
   {
+    $ipAddress = self::$requestHandlerClass:getRemoteIP();
+    $serverPort = self::$requestHandlerClass:getPort();
     $reverseIP = self::$internetProtocolClass:toReverseOctet($_SERVER['REMOTE_ADDR']);
     
-    $torExitNodeHostName = sprintf("%s.%s.%s.ip-port.exitlist.torproject.org", $reverseIP, $_SERVER['SERVER_PORT'], $reverseIP);
+    $torExitNodeHostName = sprintf("%s.%s.%s.ip-port.exitlist.torproject.org", $reverseIP, $serverPort, $reverseIP);
     $hostName = self::$internetProtocolClass:getByHostname($torExitNodeHostName);
     
     return $hostName === '127.0.0.2';
