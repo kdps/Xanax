618e88cb04a5585e9042ac202d6e9527e776f5a9
kdp <torinoyume@naver.com>
2020-01-06 19:55:32 +0900

    Update Regex.php

--- Classes/Src/Classes/Regex/Regex.php
+++ Classes/Src/Classes/Regex/Regex.php
@@ -4,5 +4,27 @@ namespace Xanax/Classes;
 
 class Regex
 {
-
+  public static function Filter(string $pattern, string $replacement, $subject)
+  {
+  }
+  
+  public static function Quote(string $string, string $delimiter = NULL)
+  {
+  }
+  
+  public static function Match(string $pattern, string $subject)
+  {
+  }
+  
+  public static function matchAll(string $pattern, string $subject)
+  {
+  }
+  
+  public static function Split(string $pattern, $subject, int $limit = -1)
+  {
+  }
+  
+  public static function Replace(string $pattern, string $replacement, $subject, int $limit = -1)
+  {
+  }
 }
