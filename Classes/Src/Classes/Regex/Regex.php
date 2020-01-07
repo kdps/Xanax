20820e96846506cac537fdd279fbb47155d76967
kdp <torinoyume@naver.com>
2020-01-07 12:53:16 +0900

    Update Regex.php

--- Classes/Src/Classes/Regex/Regex.php
+++ Classes/Src/Classes/Regex/Regex.php
@@ -2,10 +2,14 @@
 
 namespace Xanax/Classes;
 
+use Xanax/Classes/Regex/StringResult as StringResult;
+use Xanax/Classes/Regex/ArrayResult as ArrayResult;
+
 class Regex
 {
   public static function Filter(string $pattern, string $replacement, $subject)
   {
+    
   }
   
   public static function Quote(string $string, string $delimiter = NULL)
@@ -14,10 +18,16 @@ class Regex
   
   public static function Match(string $pattern, string $subject)
   {
+    $bool = @preg_match($pattern, $subject, $matches);
+    
+    return StringResult::Result($pattern, $subject, $matches, $bool);
   }
   
   public static function matchAll(string $pattern, string $subject)
   {
+    $bool = @preg_match_all($pattern, $subject, $matches);
+    
+    return ArrayResult::Result($pattern, $subject, $matches, $bool);
   }
   
   public static function Split(string $pattern, $subject, int $limit = -1)
