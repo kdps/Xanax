08ee438560f1e6c6ac87df8c3e73c9e9f469bcb8
kdp <torinoyume@naver.com>
2020-01-07 12:57:51 +0900

    Update StringResult.php

--- Classes/Src/Classes/Regex/StringResult.php
+++ Classes/Src/Classes/Regex/StringResult.php
@@ -6,4 +6,13 @@ use Xanax/Trait/Regex/Error;
 
 class StringResult
 {
+  public function __constructor(array $result)
+  {
+    $this->result = $result;
+  }
+  
+  public function Result(array $result)
+  {
+      return new static ($result);
+  }
 }
