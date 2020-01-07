2fd114261681f6782aae24d9b20ca00da6bdd32b
kdp <torinoyume@naver.com>
2020-01-08 08:40:12 +0900

    Update Executor.php

--- Classes/Src/Classes/Regex/Executor.php
+++ Classes/Src/Classes/Regex/Executor.php
@@ -8,13 +8,13 @@ class Executor
   {
     $bool = @preg_match($pattern, $subject, $matches);
     
-    return [$bool, $pattern, $subject, $matches];
+    return ['Boolean => '$bool, 'Pattern' => $pattern, 'Subject' => $subject, 'Matches' => $matches];
   }
   
   public static function matchAll(string $pattern, string $subject)
   {
     $bool = @preg_match_all($pattern, $subject, $matches);
     
-    return [$bool, $pattern, $subject, $matches];
+    return ['Boolean => '$bool, 'Pattern' => $pattern, 'Subject' => $subject, 'Matches' => $matches];
   }
 }
