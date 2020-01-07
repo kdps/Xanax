b819d553d0ac828a84530c62dac00472c4c8b764
kdp <torinoyume@naver.com>
2020-01-08 08:57:19 +0900

    Update ArrayResult.php

--- Classes/Src/Classes/Regex/ArrayResult.php
+++ Classes/Src/Classes/Regex/ArrayResult.php
@@ -8,7 +8,7 @@ class ArrayResult
 {
   public function __constructor(array $result)
   {
-    $this->boolean = $result['Boolean'] ?? "";
+    $this->boolean = $result['Boolean'] ?? false;
     $this->pattern = $result['Pattern'] ?? "";
     $this->subject = $result['Subject'] ?? "";
     $this->matches = $result['Matches'] ?? "";
@@ -21,17 +21,33 @@ class ArrayResult
   
   public function hasResult()
   {
-    return !empty($this->matches);
+    return $this->boolean;
+  }
+  
+  public function getPattern()
+  {
+    return $this->pattern;
+  }
+  
+  public function getSubject()
+  {
+    return $this->subject;
+  }
+  
+  public function getMatches()
+  {
+    return $this->matches;
   }
   
   public function getByIndex(int $index = 0)
   {
-    return $this->matches[$index] ?? "";
+    $matches = $this->getMatches();
+    return $matches[$index] ?? "";
   }
   
   public function getResults()
   {
-    return $this->matches;
+    return $this->getMatches();
   }
   
 }
