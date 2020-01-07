91fcb7cf39f2452c8d8e505d9743e63984d8dada
kdp <torinoyume@naver.com>
2020-01-07 12:58:05 +0900

    Update ArrayResult.php

--- Classes/Src/Classes/Regex/ArrayResult.php
+++ Classes/Src/Classes/Regex/ArrayResult.php
@@ -6,4 +6,13 @@ use Xanax/Trait/Regex/Error;
 
 class ArrayResult
 {
+  public function __constructor(array $result)
+  {
+    $this->result = $result;
+  }
+  
+  public function Result(array $result)
+  {
+      return new static ($result);
+  }
 }
