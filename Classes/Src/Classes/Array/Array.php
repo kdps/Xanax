f8771ad4d79f9e77f8961370609ca2c86065670b
kdp <torinoyume@naver.com>
2020-01-08 12:58:10 +0900

    Update Array.php

--- Classes/Src/Classes/Array/Array.php
+++ Classes/Src/Classes/Array/Array.php
@@ -2,4 +2,18 @@
 
 class Array
 {
+  public function getDepth(array $array)
+  {
+    $depth = 0;
+    $arrayReclusive = new RecursiveArrayIterator($array);
+    $iteratorReclusive = new RecursiveIteratorIterator($arrayReclusive);
+
+    foreach ($iteratorReclusive as $iterator) {
+        $currentDepth = $reclusive->getDepth();
+      
+        $depth = $currentDepth > $depth ? $currentDepth : $depth;
+    }
+
+    return $depth;
+  }
 }
