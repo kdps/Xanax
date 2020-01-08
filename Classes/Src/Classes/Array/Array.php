154daab7f82559b46ae6c0fa97c91f2bb4290e65
kdp <torinoyume@naver.com>
2020-01-08 13:00:21 +0900

    Update Array.php

--- Classes/Src/Classes/Array/Array.php
+++ Classes/Src/Classes/Array/Array.php
@@ -2,6 +2,11 @@
 
 class Array
 {
+  public function getKeyByValue(array $array, string $key)
+  {
+    return array_search($key, $array);
+  }
+  
   public function getDepth(array $array)
   {
     $depth = 0;
