fc77926914d26ec3ebaa78b3f76815503b31c536
kdp <kdp7584@gmail.com>
2020-02-20 13:04:27 +0900

    Update CacheControl.php

--- Classes/Src/Classes/Request/CacheControl.php
+++ Classes/Src/Classes/Request/CacheControl.php
@@ -1,4 +1,60 @@
 <?php
 
-class CacheControl extends Header {
+
+declare(strict_types=1);
+
+namespace Xanax\Classes;
+
+class CacheControl extends Header 
+{
+	
+	public function response($value)
+	{
+		$parent->responseWithKey('Cache-Control', $value);
+	}
+	
+	public function responseMinFresh($value)
+	{
+		$key = "min-fresh";
+		$data = "$key=$value";
+		
+		$this->response($data);
+	}
+	
+	public function responseMaxStale($value)
+	{
+		$key = "max-stale";
+		$data = "$key=[=$value]";
+		
+		$this->response($data);
+	}
+	
+	public function responseMaxAge($value)
+	{
+		$key = "max-age";
+		$data = "$key=$value";
+		
+		$this->response($data);
+	}
+	
+	public function responseOnlyIfCached()
+	{
+		$this->response('only-if-cached');
+	}
+	
+	public function responseNoStore()
+	{
+		$this->response('no-store');
+	}
+	
+	public function responseNoTransform()
+	{
+		$this->response('no-transform');
+	}
+	
+	public function responseNoCache()
+	{
+		$this->response('no-cache');
+	}
+	
 }
