65ef3c91b9840a2667058a6872d258e198b7b532
kdp <kdp7584@gmail.com>
2020-03-03 12:25:27 +0900

    Update XMLHandler.php

--- Classes/Src/Classes/XML/XMLHandler.php
+++ Classes/Src/Classes/XML/XMLHandler.php
@@ -4,4 +4,23 @@ namespace Xanax/Classes;
 
 class XMLHandler
 {
+	
+	private $data;
+	
+	public function Parse($text) {
+		$this->data = simplexml_load_string($text);
+	}
+	
+	public function isValid() {
+		if ($this->data == null || !$this->data) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public function getChildren() {
+		return $this->data->children();
+	}
+	
 }
