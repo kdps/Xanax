7f3d44b8df3269f7733bb32a043734c80a7cfc1e
kdp <kdp7584@gmail.com>
2020-03-03 19:25:23 +0900

    Update File.php

--- Classes/Src/Classes/Protocol/PHP/File.php
+++ Classes/Src/Classes/Protocol/PHP/File.php
@@ -4,6 +4,22 @@ namespace Xanax/Classes/Protocol/PHP;
 
 class PHPProtocol {
 
+	public const _STANDARD_ERROR_ = "php://stderr";
+	
+	public const _STANDARD_OUTPUT_ = "php://stdout";
+		
+	public const _STANDARD_INPUT_ = "php://stdin";
+	
+	public const _FILTER_ = "php://filter";
+	
+	public const _TEMPORARY_ = "php://temp";
+	
+	public const _MEMORY_ = "php://memory";
+	
+	public const _INPUT_ = "php://input";
+	
+	public const _OUTPUT_ = "php://output";
+	
 	public function getStandardError() {
 		return "php://stderr";
 	}
