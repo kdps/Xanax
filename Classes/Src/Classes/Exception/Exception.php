3cf35e6ab364c6c3e025140ee5450997f47c1179
kdp <kdp7584@gmail.com>
2020-03-05 13:01:04 +0900

    Update Exception.php

--- Classes/Src/Classes/Exception/Exception.php
+++ Classes/Src/Classes/Exception/Exception.php
@@ -4,10 +4,8 @@ class Exception {
 
 	public function setErrorHandler($errorRaised, $errorMessage, $fileName, $lineNumber, $context, callable $callback) {
 		$previous = set_error_handler(function ($errorRaised, $errorMessage, $fileName, $lineNumber, $context) use (&$previous) {
-			if ($previous) {
-				if ($callback instanceof callable) {
-					$callback($errorRaised, $errorMessage, $fileName, $lineNumber, $context);
-				}
+			if ($previous && $callback instanceof callable) {
+				$callback($errorRaised, $errorMessage, $fileName, $lineNumber, $context);
 			} else {
 				return false;
 			}
