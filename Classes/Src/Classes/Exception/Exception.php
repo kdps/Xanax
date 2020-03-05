fd3f82525b6ec9c21a2d135f534ac70b8f5221e4
kdp <kdp7584@gmail.com>
2020-03-05 12:59:27 +0900

    Create Exception.php

---
+++ Classes/Src/Classes/Exception/Exception.php
@@ -0,0 +1,15 @@
+<?php
+
+class Exception {
+
+	public function setErrorHandler($errorRaised, $errorMessage, $fileName, $lineNumber, $context, callable $callback) {
+		$previous = set_error_handler(function ($errorRaised, $errorMessage, $fileName, $lineNumber, $context) use (&$previous) {
+			if ($previous) {
+				if ($callback insteadof callable) {
+					$callback($errorRaised, $errorMessage, $fileName, $lineNumber, $context);
+				}
+			}
+		});
+	}
+	
+}
