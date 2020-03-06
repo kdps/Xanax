6e8a3e68e3b8fd85bb0a48e48733e537bc8b3c2a
kdp <kdp7584@gmail.com>
2020-03-06 09:03:30 +0900

    Update Data.php

--- Classes/Src/Classes/Data/Data.php
+++ Classes/Src/Classes/Data/Data.php
@@ -6,11 +6,31 @@ use Xanax/Classes/Data/Identifier as Identifier;
 
 class Data
 {
-  private static $data;
+	  private static $data;
   
-  public function __constructor($data)
-  {
-    self::$data = $data;
-  }
+	public function __constructor($data)
+	{
+		self::$data = $data;
+	}
+
+	public function typeCasting($type) {
+		switch($type) {
+			case "string":
+				self::data = (string)self::data;
+				break;
+			case "integer":
+				self::data = (integer)self::data;
+				break;
+			case "float":
+				self::data = (float)self::data;
+				break;
+			case "boolean":
+				self::data = (boolean)self::data;
+				break;
+			case "array":
+				self::data = (array)self::data;
+				break;
+		}
+	}
   
 }
