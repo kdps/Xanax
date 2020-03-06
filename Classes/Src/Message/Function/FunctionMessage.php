6aac352515a6e7f8c5b941b11b7279ab43218404
kdp <kdp7584@gmail.com>
2020-03-06 12:40:31 +0900

    Update FunctionMessage.php

--- Classes/Src/Message/Function/FunctionMessage.php
+++ Classes/Src/Message/Function/FunctionMessage.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Xanax\Message\FileHandler;
+namespace Xanax\Message\Function;
 
-class FileHandlerMessage
+class FunctionMessage
 {
 	
 	public static function getFunctionIsNotFileMessage()
