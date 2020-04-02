830fc32b5a21f548b99723a87a8450e6e2574937
kdp <kdp7584@gmail.com>
2020-04-02 17:48:31 +0900

    Update FirebaseCloudMessaging.php

--- Plugins/API/FirebaseCloudMessaging.php
+++ Plugins/API/FirebaseCloudMessaging.php
@@ -1,32 +1,65 @@
 <?php
 
 class FirebaseCloudMessaging {
-  public function sendToChannel($token, $title, $body, $channelIdentify, $clickAction) {
-    $postData = array(
-      'to' => $token,
-      'notification' => array(
-        'title' => $title,
-        'body' => $body,
-        'android_channel_id' => $channelId,
-        'click_action' => $clickAction
-      )
-    );
 
-    $headers = array(
-      'Authorization: key=',
-      'Content-Type: application/json'
-    );
+	public function sendMessage($registrationIds, $data, $title, $body) {
+		$fields = array(
+			'registration_ids'  => $registrationIds,
+			'data'              => $data,
+			'content_available' => true,
+			'priority'          => 'high',
+			'notification'      => array(
+				"title" => $title, 
+				"body" => $body, 
+				"sound" => "default"
+			)
+		);
 
-    $ch = curl_init();
-    curl_setopt($ch,CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
-    curl_setopt($ch,CURLOPT_POST, true);
-    curl_setopt($ch,CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, true);
-    curl_setopt($ch,CURLOPT_POSTFIELDS, json_encode($postData) );
-    $result = curl_exec($ch);
-    curl_close($c);
+		$headers = array(
+			'Authorization: key=',
+			'Content-Type: application/json'
+		);
 
-    return $result;
-  }
+		$ch = curl_init();
+
+		curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($fields));
+		$result = curl_exec($ch);
+		curl_close($c);
+
+		return $result;
+	}
+
+	public function sendToChannel($token, $title, $body, $channelIdentify, $clickAction) {
+		$postData = array(
+			'to' => $token,
+			'notification' => array(
+				'title' => $title,
+				'body' => $body,
+				'android_channel_id' => $channelId,
+				'click_action' => $clickAction
+			)
+		);
+
+		$headers = array(
+			'Authorization: key=',
+			'Content-Type: application/json'
+		);
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($postData));
+		$result = curl_exec($ch);
+		curl_close($c);
+
+		return $result;
+	}
+
 }
