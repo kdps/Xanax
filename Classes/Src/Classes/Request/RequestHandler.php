6df8cfea4c472178fe1b13be3b9813af2d496d4d
kdp <kdp7584@gmail.com>
2020-04-04 12:10:00 +0900

    Update RequestHandler.php

--- Classes/Src/Classes/Request/RequestHandler.php
+++ Classes/Src/Classes/Request/RequestHandler.php
@@ -82,6 +82,22 @@ class RequestHandler
 		return $_SERVER['REQUEST_SCHEME'];
 	}
 	
+	public function isValidInternetProtocol(string $internetProtocol, string $type = ''): bool {
+		switch (strtolower($type)) {
+			case 'ipv4':
+				$filter = FILTER_FLAG_IPV4;
+				break;
+			case 'ipv6':
+				$filter = FILTER_FLAG_IPV6;
+				break;
+			default:
+				$filter = null;
+				break;
+		}
+
+		return boolval(filter_var($internetProtocol, FILTER_VALIDATE_IP, $filter));
+	}
+	
 	public function getProtocol() :string
 	{
 		return substr(strtolower($_SERVER['SERVER_PROTOCOL']), 0, strpos(strtolower($_SERVER['SERVER_PROTOCOL']), '/'));
