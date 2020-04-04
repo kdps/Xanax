37395fc75d6eba3ea0020b3a6fdf1622862619ca
kdp <kdp7584@gmail.com>
2020-04-04 12:11:49 +0900

    Create Internet.php

---
+++ Classes/Src/Classes/Protocol/Internet.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Xanax/Classes/Protocol;
+
+class Internet {
+  
+	public function isValid(string $internetProtocol, string $type = ''): bool {
+		switch (strtolower($type)) {
+			case 'ipv4':
+				$filter = FILTER_FLAG_IPV4;
+				break;
+			case 'ipv6':
+				$filter = FILTER_FLAG_IPV6;
+				break;
+			default:
+				$filter = null;
+				break;
+		}
+
+		return boolval(filter_var($internetProtocol, FILTER_VALIDATE_IP, $filter));
+	}
+	
+}
