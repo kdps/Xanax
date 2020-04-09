88ef6c1bd23e158f313aa24a9c35a7ce5528985d
kdp <kdp7584@gmail.com>
2020-04-09 13:20:11 +0900

    Update Owner.php

--- Classes/Src/Classes/Permission/Owner.php
+++ Classes/Src/Classes/Permission/Owner.php
@@ -12,4 +12,14 @@ class Owner extends Permission
     self::$mode = parent::getMode();
   }
   
+  public function isReadable()
+  {
+    return (self::$mode & 0x0100);
+  }
+  
+  public function isWritable()
+  {
+    return (self::$mode & 0x0080);
+  }
+  
 }
