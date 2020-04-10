70773c1c9c451bef031333c82d771161fd3d784c
kdp <kdp7584@gmail.com>
2020-04-10 12:46:53 +0900

    Delete Interpreter.php

--- Example/Interpreter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-include("./../vendor/autoload.php");
-
-use Xanax\Classes\Tokenizer as Tokenizer;
-
-$tokenizer = new Tokenizer();
-
-$tokenizer->generateToken(file_get_contents("script.txt"));
