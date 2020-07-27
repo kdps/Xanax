20ba7b6c98697a69156b3a35db635911721e73f8
kdp <kdp7584@gmail.com>
2020-07-27 21:44:09 +0900

    Create Builder.php

---
+++ Classes/Src/Classes/Regex/Builder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Xanax/Classes/Regex;
+
+use Xanax/Trait/Regex/Error;
+
+class Builder
+{
+  public function positiveLookbehind($regex) {
+    return "(?<=(${regex}))";
+  }
+  
+  public function negativeLookbehind($regex) {
+    return "(?<!(${regex}))";
+  }
+  
+  public function positiveLookahead($regex) {
+    return "(?=(${regex}))";
+  }
+  
+  public function negativeLookahead($regex) {
+    return "(?!(${regex}))";
+  }
+
+}
