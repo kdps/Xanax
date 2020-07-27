7e68a53ea15a850ada9bbcbd52bea4f0fa976289
kdp <kdp7584@gmail.com>
2020-07-27 22:44:40 +0900

    Update Builder.php

--- Classes/Src/Classes/Regex/Builder.php
+++ Classes/Src/Classes/Regex/Builder.php
@@ -12,6 +12,10 @@ class Builder
     return "[a-zA-Z_\x7f-\xff]";
   }
   
+  public function setComment($content) {
+    return "(?#${content})"; 
+  }
+  
   public function unicodeCategory() {
     return "\p{L}"
   }
