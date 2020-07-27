5b1ecd80084ce4142a57cf35dc1dcc4c8ad7dd25
kdp <kdp7584@gmail.com>
2020-07-27 22:56:27 +0900

    Update Builder.php

--- Classes/Src/Classes/Regex/Builder.php
+++ Classes/Src/Classes/Regex/Builder.php
@@ -12,6 +12,10 @@ class Builder
     return "[a-zA-Z_\x7f-\xff]";
   }
   
+  public function Condition($expression, $then, $else) {
+    return "(?(?=${expression})${then}|${else})";
+  }
+  
   public function turnOnFreeSpacingMode() {
     return "(?x)";  
   }
