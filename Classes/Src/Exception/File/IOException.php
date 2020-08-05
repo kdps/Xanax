dfd78791031d87801609070331ec17045c2c7ea4
kdp <kdp7584@gmail.com>
2020-08-05 18:23:21 +0900

    Update IOException.php

--- Classes/Src/Exception/File/IOException.php
+++ Classes/Src/Exception/File/IOException.php
@@ -2,10 +2,10 @@
 
 namespace Xanax\Exception\FileHandler;
 
-class IOException extends \RuntimeException
-{
-	public function __construct(string $message, int $code = 0, \Exception $previous = null)
-	{
+class IOException extends \RuntimeException {
+	
+	public function __construct(string $message, int $code = 0, \Exception $previous = null) {
 		parent::__construct($message, $code, $previous);
 	}
+	
 }
