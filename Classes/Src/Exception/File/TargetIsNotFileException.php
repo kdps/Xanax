24e911a51cfc8a35f83a6c8129e16c2ae9fa9519
kdp <kdp7584@gmail.com>
2020-08-05 18:23:46 +0900

    Update TargetIsNotFileException.php

--- Classes/Src/Exception/File/TargetIsNotFileException.php
+++ Classes/Src/Exception/File/TargetIsNotFileException.php
@@ -2,10 +2,10 @@
 
 namespace Xanax\Exception\FileHandler;
 
-class TargetIsNotFileException extends IOException
-{
-	public function __construct(string $message = null, int $code = 0, \Exception $previous = null)
-	{
+class TargetIsNotFileException extends IOException {
+	
+	public function __construct(string $message = null, int $code = 0, \Exception $previous = null) {
 		parent::__construct($message, $code, $previous);
 	}
+	
 }
