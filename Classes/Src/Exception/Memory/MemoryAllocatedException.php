9ab0fb707f1e2a52153b735c21aaa10a7a9b0a65
kdp <kdp7584@gmail.com>
2020-08-05 18:24:00 +0900

    Update MemoryAllocatedException.php

--- Classes/Src/Exception/Memory/MemoryAllocatedException.php
+++ Classes/Src/Exception/Memory/MemoryAllocatedException.php
@@ -2,10 +2,10 @@
 
 namespace Xanax\Exception\FileHandler;
 
-class MemoryAllocatedException extends MemoryException
-{
-	public function __construct(string $message = null, int $code = 0, \Exception $previous = null)
-	{
+class MemoryAllocatedException extends MemoryException {
+	
+	public function __construct(string $message = null, int $code = 0, \Exception $previous = null) {
 		parent::__construct($message, $code, $previous);
 	}
+	
 }
