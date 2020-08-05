ddda82169df4578eb8d95204d2b0b60ebc467ce2
kdp <kdp7584@gmail.com>
2020-08-05 18:24:19 +0900

    Update MemoryException.php

--- Classes/Src/Exception/Memory/MemoryException.php
+++ Classes/Src/Exception/Memory/MemoryException.php
@@ -2,10 +2,10 @@
 
 namespace Xanax\Exception\FileHandler;
 
-class MemoryException extends \RuntimeException
-{
-	public function __construct(string $message, int $code = 0, \Exception $previous = null)
-	{
+class MemoryException extends \RuntimeException {
+	
+	public function __construct(string $message, int $code = 0, \Exception $previous = null) {
 		parent::__construct($message, $code, $previous);
 	}
+	
 }
