3552a835f76c8b750ff9b5f363fada7ed2d81072
kdp <kdp7584@gmail.com>
2020-08-05 18:35:19 +0900

    Update InternetProtocol.php

--- Classes/Src/Classes/Web/InternetProtocol.php
+++ Classes/Src/Classes/Web/InternetProtocol.php
@@ -4,14 +4,14 @@ namespace Xanax/Classes;
 
 class InternetProtocol {
 
-  public function getByHostname($hostname) {
-    return getbyhostname($hostname);
-  }
+	public function getByHostname($hostname) {
+		return getbyhostname($hostname);
+	}
 
-  public function toReverseOctet($inputip) {
-    $ipoc = explode(".", $inputip);
+	public function toReverseOctet($inputip) {
+		$ipoc = explode(".", $inputip);
 
-    return $ipoc[3].".".$ipoc[2].".".$ipoc[1].".".$ipoc[0];
-  }
+		return $ipoc[3].".".$ipoc[2].".".$ipoc[1].".".$ipoc[0];
+	}
 
 }
