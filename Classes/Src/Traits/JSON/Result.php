8ef41657185f8896aff412e9353081301a5becd2
kdp <kdp7584@gmail.com>
2020-08-10 19:55:50 +0900

    Create Result.php

---
+++ Classes/Src/Traits/JSON/Result.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Xanax/Trait/Json;
+trait Result
+{
+
+	public function getLastError() {
+		return json_last_error();
+	}
+	
+	public function hasError() {
+		return $this->getLastError() !== JSON_ERROR_NONE;
+	}
+	
+	public function hasSyntaxError() {
+		return $this->getLastError() !== JSON_ERROR_SYNTAX;
+	}
+	
+	public function isMalformed() {
+		// TODO
+		return $this->getLastError() === JSON_ERROR_STATE_MISMATCH;
+	}
+	
+	public function isInvalid() {
+		// TODO
+		return $this->getLastError() === JSON_ERROR_STATE_MISMATCH;
+	}
+	
+	public function isStackDepthExceeded() {
+		return $this->getLastError() === JSON_ERROR_DEPTH;
+	}
+	
+	// PHP 7
+	
+	public function isMalformedUTF8() {
+		return $this->getLastError() === JSON_ERROR_UTF8;
+	}
+		
+}
