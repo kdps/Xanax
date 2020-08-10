7c201a4a47eda23a39798decf41406f9b1f382c6
kdp <kdp7584@gmail.com>
2020-08-10 19:02:43 +0900

    Update Date.php

--- Classes/Src/Classes/Date/Date.php
+++ Classes/Src/Classes/Date/Date.php
@@ -9,20 +9,28 @@ class Date
     $this->date = $date;
   }
   
-  public function toTime() {
-    $this->date = strtotime($date);
+  public function stringToTime($string, $format) {
+    return strtotime($string, $format);
   }
   
-  public function toString() {
-    $this->date = strftime($this->date);
+  public function getDateArray() {
+    return getdate($this->date);
+  }
+  
+  public function parseString($format) {
+    return strptime($this->date, $format);
+  }
+  
+  public function timeToString() {
+    return strftime($this->date);
   }
   
   public function getTime() {
     return time();
   }
   
-  public function getDayOfWeek() {
-    $dayofweek = date('w', $this->date);
+  public function getDayOfWeek($date) {
+    $dayofweek = date('w', $date);
     
     return $dayofweek;
   }
