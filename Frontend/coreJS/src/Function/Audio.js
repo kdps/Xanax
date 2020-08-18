36c318f72d2f95f199d4bb4ff07306df02671274
kdp <kdp7584@gmail.com>
2020-08-18 18:56:36 +0900

    Update Audio.js

--- Frontend/coreJS/src/Function/Audio.js
+++ Frontend/coreJS/src/Function/Audio.js
@@ -43,6 +43,41 @@ import AudioContextObject from './Class/AudioContextObject.js';
 				{44100, 48000, 32000, 0}
 			];*/
 			
+			this.const midiInstruments = [
+				{"Instrument" : "Acoustic Grand Piano", "Class" : "Piano"},
+				{"Instrument" : "Bright Acoustic Piano", "Class" : "Piano"},
+				{"Instrument" : "Electric Grand Piano", "Class" : "Piano"},
+				{"Instrument" : "Honky-tonk Piano", "Class" : "Piano"},
+				{"Instrument" : "Rhodes Piano", "Class" : "Piano"},
+				{"Instrument" : "Chorused Piano", "Class" : "Piano"},
+				{"Instrument" : "Harpsichord", "Class" : "Piano"},
+				{"Instrument" : "Clavinet", "Class" : "Piano"},
+				{"Instrument" : "Celesta", "Class" : "Chromatic Percussion"},
+				{"Instrument" : "Glockenspiel", "Class" : "Chromatic Percussion"},
+				{"Instrument" : "Music box", "Class" : "Chromatic Percussion"},
+				{"Instrument" : "Vibraphone", "Class" : "Chromatic Percussion"},
+				{"Instrument" : "Marimba", "Class" : "Chromatic Percussion"},
+				{"Instrument" : "Xylophone", "Class" : "Chromatic Percussion"},
+				{"Instrument" : "Tubular Bells", "Class" : "Chromatic Percussion"},
+				{"Instrument" : "Dulcimer", "Class" : "Chromatic Percussion"},
+				{"Instrument" : "Hammond Organ", "Class" : "Organ"},
+				{"Instrument" : "Percussive Organ", "Class" : "Organ"},
+				{"Instrument" : "Rock Organ", "Class" : "Organ"},
+				{"Instrument" : "Church Organ", "Class" : "Organ"},
+				{"Instrument" : "Reed Organ", "Class" : "Organ"},
+				{"Instrument" : "Accordion", "Class" : "Organ"},
+				{"Instrument" : "Harmonica", "Class" : "Organ"},
+				{"Instrument" : "Tango Accordion", "Class" : "Organ"},
+				{"Instrument" : "Acoustic Guitar (nylon)", "Class" : "Guitar"},
+				{"Instrument" : "Acoustic Guitar (steel)", "Class" : "Guitar"},
+				{"Instrument" : "Electric Guitar (jazz)", "Class" : "Guitar"},
+				{"Instrument" : "Electric Guitar (clean)", "Class" : "Guitar"},
+				{"Instrument" : "Electric Guitar (muted)", "Class" : "Guitar"},
+				{"Instrument" : "Overdriven Guitar", "Class" : "Guitar"},
+				{"Instrument" : "Distortion Guitar", "Class" : "Guitar"},
+				{"Instrument" : "Guitar Harmonics", "Class" : "Guitar"},
+			];
+			
 			this.musicGenre = ["Blues", "Classic Rock", "Country", "Dance", "Disco", "Funk", "Grunge", "Hip-Hop", "Jazz", "Metal", "New Age", "Oldies", "Other", "Pop", "R&B", "Rap", "Reggae", "Rock", "Techno", "Industrial", "Alternative", "Ska", "Death Metal", "Pranks", "Soundtrack", "Euro-Techno", "Ambient", "Trip-Hop", "Vocal", "Jazz+Funk", "Fusion", "Trance", "Classical", "Instrumental", "Acid", "House", "Game", "Sound Clip", "Gospel", "Noise", "AlternRock", "Bass", "Soul", "Punk", "Space", "Meditative", "Instrumental Pop", "Instrumental Rock", "Ethnic", "Gothic", "Darkwave", "Techno-Industrial", "Electronic", "Pop-Folk", "Eurodance", "Dream", "Southern Rock", "Comedy", "Cult", "Gangsta Rap", "Top 40", "Christian Rap", "Pop / Funk", "Jungle", "Native American", "Cabaret", "New Wave", "Psychedelic", "Rave", "Showtunes", "Trailer", "Lo-Fi", "Tribal", "Acid Punk", "Acid Jazz", "Polka", "Retro", "Musical", "Rock & Roll", "Hard Rock", "Folk", "Folk-Rock", "National Folk", "Swing", "Fast  Fusion", "Bebob", "Latin", "Revival", "Celtic", "Bluegrass", "Avantgarde", "Gothic Rock", "Progressive Rock", "Psychedelic Rock", "Symphonic Rock", "Slow Rock", "Big Band", "Chorus", "Easy Listening", "Acoustic", "Humour", "Speech", "Chanson", "Opera", "Chamber Music", "Sonata", "Symphony", "Booty Bass", "Primus", "Porn Groove", "Satire", "Slow Jam", "Club", "Tango", "Samba", "Folklore", "Ballad", "Power Ballad", "Rhythmic Soul", "Freestyle", "Duet", "Punk Rock", "Drum Solo", "A Cappella", "Euro-House", "Dance Hall", "Goa", "Drum & Bass", "Club-House", "Hardcore", "Terror", "Indie", "BritPop", "Negerpunk", "Polsk Punk", "Beat", "Christian Gangsta Rap", "Heavy Metal", "Black Metal", "Crossover", "Contemporary Christian", "Christian Rock", "Merengue", "Salsa", "Thrash Metal", "Anime", "JPop", "Synthpop", "Rock/Pop"];
 			
 			this.StreamAudio = null;
