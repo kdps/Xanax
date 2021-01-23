623f73b9e166e5943bcdd7db4ac30223e7525821
kdp <kdp7584@gmail.com>
2021-01-23 13:43:57 +0900

    Update Object.php

--- Classes/Src/Classes/File/Object.php
+++ Classes/Src/Classes/File/Object.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Xanax\Classes;
+namespace Xanax\Classes\File;
 
 use Xanax\Implement\FileObjectInterface;
 use Xanax\Exception\FileHandler\FileIsNotExistsException;
