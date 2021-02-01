2f58f1918945e66caf32ab19377cb17462f5556b
kdp <kdp7584@gmail.com>
2021-02-01 18:56:50 +0900

    Update and rename InvalidTypeHandler.php to InvalidTypeException.php

--- Classes/Src/Exception/Resource/InvalidTypeException.php
+++ Classes/Src/Exception/Resource/InvalidTypeException.php
@@ -2,7 +2,7 @@
 
 namespace Xanax\Exception\ResourceHandler;
 
-class InvalidTypeHandler extends IOException {
+class InvalidTypeException extends IOException {
 	
 	public function __construct(string $message = null, int $code = 0, \Exception $previous = null) {
 		parent::__construct($message, $code, $previous);
