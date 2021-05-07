14ee02d0c44450b286c1c647274453031955c93f
kdp <kdp7584@gmail.com>
2021-05-07 20:33:57 +0900

    Update JSONHandler.php

--- Classes/Src/Classes/String/JSONHandler.php
+++ Classes/Src/Classes/String/JSONHandler.php
@@ -2,17 +2,21 @@
 
 namespace Xanax\Classes\String;
 
-class JSONHandler {
+class JSONHandler 
+{
 	
-	public function Decode($string) {
+	public function Decode($string) 
+	{
 		return json_decode($string);
 	}
 
-	public function Encode($string) {
+	public function Encode($string) 
+	{
 		return json_encode($string, JSON_PRETTY_PRINT);
 	}
 
-	public function isJSON($string) {
+	public function isJSON($string) 
+	{
 		return is_string($string) && is_array(json_decode($string, true)) && (json_last_error() == JSON_ERROR_NONE) ? true : false;
 	}
 	
