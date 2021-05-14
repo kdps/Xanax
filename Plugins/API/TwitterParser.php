4c1a49a082365eafd856cabd9c8165fb9ad74a27
kdp <kdp7584@gmail.com>
2021-05-14 23:00:05 +0900

    Update TwitterParser.php

--- Plugins/API/TwitterParser.php
+++ Plugins/API/TwitterParser.php
@@ -48,4 +48,8 @@ class TwitterParser {
     return json_decode($result)->data->user->result;
   }
   
+  public function getVideoUrl($id) {
+    return $videoUrl = "https://twitter.com/i/api/2/timeline/conversation/".$id.".json?include_profile_interstitial_type=1&include_blocking=1&include_blocked_by=1&include_followed_by=1&include_want_retweets=1&include_mute_edge=1&include_can_dm=1&include_can_media_tag=1&skip_status=1&cards_platform=Web-12&include_cards=1&include_ext_alt_text=true&include_quote_count=true&include_reply_count=1&tweet_mode=extended&include_entities=true&include_user_entities=true&include_ext_media_color=true&include_ext_media_availability=true&send_error_codes=true&simple_quoted_tweet=true&count=50&include_ext_has_birdwatch_notes=false&ext=mediaStats%2ChighlightedLabel";
+  }
+ 
 }
