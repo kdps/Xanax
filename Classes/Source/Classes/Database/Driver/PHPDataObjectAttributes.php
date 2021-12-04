85ad799b72f601934666d9a4a612df4b5bdab7f1
kdp <kdp7584@gmail.com>
2021-12-04 21:43:47 +0900

    Add PDO Options

--- Classes/Source/Classes/Database/Driver/PHPDataObjectAttributes.php
+++ Classes/Source/Classes/Database/Driver/PHPDataObjectAttributes.php
@@ -52,6 +52,7 @@ class PHPDataObjectAttributes {
         $this->setAttribute(\PDO::ATTR_EMULATE_PREPARES, $bool);
     }
 
+    // If this attribute is set to true on a PDOStatement, the MySQL driver will use the buffered versions of the MySQL API.
     public function useBufferedQueries(bool $bool) {
         $this->setAttribute(\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, $bool);
     }
@@ -68,13 +69,47 @@ class PHPDataObjectAttributes {
     public function setAssociativeArraySameColumnNameFetch(bool $bool) {
         $this->setDefaultFetchMode(\PDO::FETCH_NAMED, $bool);
     }
-
+    
     public function setColunNumberFetch(bool $bool) {
         $this->setDefaultFetchMode(\PDO::FETCH_NUM, $bool);
     }
 
     public function setPredefinedClassFetch(bool $bool) {
         $this->setDefaultFetchMode(\PDO::FETCH_OBJ, $bool);
+    }
+
+    // Command to execute when connecting to the MySQL server. Will automatically be re-executed when reconnecting.
+    public function setConnectionCommand($string) {
+        $this->setAttribute(\PDO::MYSQL_ATTR_INIT_COMMAND, $string);
+    }
+
+    // The file path to the SSL certificate.
+    public function setSSLCertificateFilePath($filepath) {
+        $this->setAttribute(\PDO::MYSQL_ATTR_SSL_CERT, $filepath);
+    }
+
+    // The file path to the directory that contains the trusted SSL CA certificates, which are stored in PEM format.
+    public function setTrustedSSLCACertificateFilePath($filepath) {
+        $this->setAttribute(\PDO::MYSQL_ATTR_SSL_CAPATH, $filepath);
+    }
+
+    // The file path to the SSL key.
+    public function setSSLCertificateKeyFilePath($filepath) {
+        $this->setAttribute(\PDO::MYSQL_ATTR_SSL_KEY, $filepath);
+    }
+
+    // Enable network communication compression.
+    public function setNetworkCommunicationCompression($enable) {
+        $this->setAttribute(\PDO::MYSQL_ATTR_COMPRESS, $enable);
+    }
+
+    public function setSSLEncryptionCipher($cipher) {
+        $this->setAttribute(\PDO::MYSQL_ATTR_SSL_CIPHER, $cipher);
+    }
+
+    // Disables multi query execution
+    public function useMultiQueryExecution($size) {
+        $this->setAttribute(\PDO::MYSQL_ATTR_MULTI_STATEMENTS, $size);
     }
 
 }
