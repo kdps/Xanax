52dac52e6080b32bac5907d3e695b5d633de012c
kdp <kdp7584@gmail.com>
2022-01-02 22:11:36 +0900

    Create Math.php

---
+++ Classes/Source/Classes/Math/Math.php
@@ -0,0 +1,24 @@
+<?php
+
+class Math {
+ 
+  public function sigmoid($x) {
+    return 1 / (1 + exp(-$x));
+  }
+  
+  public function tanh($x) {
+    if (function_exists('tanh')) {
+      return tanh($x);
+    }
+    
+    return (exp($x) - exp(-$x)) / (exp($x) + exp(-$x));
+  }
+  
+  public function relu($x) {
+    
+  }
+  
+  public function gelu($x) {
+  }
+  
+}
