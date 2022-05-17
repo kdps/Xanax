ee704b5538568eca2d176741900fe55499ffc12a
kdp <kdp7584@gmail.com>
2022-05-17 19:50:48 +0900

    Create Route.php

---
+++ Classes/Source/Classes/HTTP/Route.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Xanax\Classes\HTTP;
+
+use Xanax\Enumeration\HTTPRequestMethod;
+
+class Request 
+{
+  private $routes = array();
+  
+  public function Get() 
+  {
+  }
+  
+  public function Post() 
+  {
+  }
+  
+  public function Delete() 
+  {
+  }
+  
+  public function Put() 
+  {
+  }
+  
+  public function Options() 
+  {
+  }
+  
+  public function Patch() 
+  {
+  }
+  
+  public function Patch() 
+  {
+  }
+  
+}
